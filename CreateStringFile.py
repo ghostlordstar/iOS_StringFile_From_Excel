a7a3696d5c6787b95114b38219c952d657f88b81
walker <zhangheshan@funlink-tech.com>
2021-02-01 18:22:00 +0800

    [GL][对国际化文案进行去重处理]

--- CreateStringFile.py
+++ CreateStringFile.py
@@ -4,6 +4,9 @@
 import time
 from openpyxl import load_workbook
 
+keys = []
+allStringDict = {}
+
 def logo():
     print("""
         
@@ -17,7 +20,6 @@ def logo():
                                                                                   |___/ 
         
     """)
-
 
 # 打开excel
 def loadExcel(excelFilePath):
@@ -34,23 +36,36 @@ def filterSheetNames(filtration,sheetNames):
     return newNames
 
 
-# 读取sheet并写入文件
-def writeStringFileFromSheet(sheetName, wb):
+# 读取sheet并处理所有文案， 并将处理后的文案保存到`allStringDict`， 将所有key保存到`keys`
+def processAllStringList(sheetName, wb):
     tmpSheet = wb[sheetName] # 取出指定的sheet
     tmpSheetMaxRow = tmpSheet.max_row
     for row in range(tmpSheetMaxRow):
         keyIndex = "B%s"%(row + 2)  # 从第二行开始读取
         valueIndex = "C%s"%(row + 2)
-        key = tmpSheet[keyIndex].value
-        value = tmpSheet[valueIndex].value
-        if key != None and len(key) > 0 and value != None and len(value) > 0:   # key和value都不为空时写入文件
-            stringFile.write('\"%s\" = \"%s\";\n' %(key, value))
+        key = "%s"%tmpSheet[keyIndex].value
+        value = "%s"%tmpSheet[valueIndex].value
+
+        if key != "None" and len(key) > 0 and value != None and len(value) > 0:   # key和value都不为空时写入文件
+            if (key in keys) == False:
+                keys.append(key)
+            else:
+                keys.remove(key)    # 删掉原来的key
+                keys.append(key)    # 将key添加到尾部
+            allStringDict[key] = value
+
+# 将处理好的文件写入国际化文件
+def writeAllStringToIntenationalFile():
+            for key in keys:
+                value = allStringDict[key]
+                if len(value) > 0:
+                    stringFile.write('\"%s\" = \"%s\";\n' %(key, value))
 
 
-# 把指定sheet文案写入国际化文件
+# 把指定sheet文案进行处理
 def writeStringFromSheets(sheets, wb):
     for sheet in sheets:
-        writeStringFileFromSheet(sheet, wb)
+        processAllStringList(sheet, wb)
 
 
 # 创建文件并添加文件头
@@ -59,7 +74,7 @@ def openAndInitializeStringFile(path):
     if path != None and len(path) > 0:
         tmpStringFile = open(path, 'w')
     else:
-        tmpStringFile = open('/Users/apple/Desktop/Localizable.strings', 'w')
+        tmpStringFile = open('/Users/walker/Desktop/Localizable.strings', 'w')
 
     tmpStringFile.write("""
 /* 
@@ -69,7 +84,8 @@ def openAndInitializeStringFile(path):
   Created by walker on %s.
   Copyright © 2021 LFG. All rights reserved.
 */
-    """%time.strftime("%Y/%m/%d %H:%M", time.localtime()))
+
+"""%time.strftime("%Y/%m/%d %H:%M", time.localtime()))
     return tmpStringFile
 
 if __name__ == "__main__":
@@ -77,7 +93,7 @@ if __name__ == "__main__":
     logo()
 
     # 读取excel文件
-    excel = loadExcel('/Users/apple/Desktop/intenational_test.xlsx')
+    excel = loadExcel('/Users/walker/Desktop/intenational_test.xlsx')
 
     # 过滤不需要的sheet
     needProcessSheetNames = filterSheetNames(['what\'s new','Backend'], excel.sheetnames)
@@ -85,10 +101,11 @@ if __name__ == "__main__":
     # 创建国际化文件
     stringFile = openAndInitializeStringFile(None)
 
-    # 将指定sheets写入到StringFile
+    # 将指定sheets进行处理，去重
     writeStringFromSheets(needProcessSheetNames, excel)
 
-    # TODO: 去重
+    # 将处理好的文案写入文件
+    writeAllStringToIntenationalFile()
 
     # 关闭文件
     stringFile.close()
